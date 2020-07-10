73f94bfb1d87ae881d7b14fcc32c909a31275df7
Mykhailo Logvynets <mlogvynets@e1r7p14.unit.ua>
2020-07-10 13:36:34 +0300

    sprint02

---
+++ sprint02/t11/mx_max.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+
+int mx_max(int a, int b, int c) {
+	if (a>=b && a>=c) {
+		return a;
+	}
+	else if (b>=a && b>=c ) {
+		return b;
+	}
+	else 
+		return c;
+}
+/*int main() {
+	printf("%d", mx_max(7, 8, 12));
+}
+*/
